9b4040de351d5d00a4d61fc8ee33eb384c48d339
Shahidul Deowan <deowanshahidul@gmail.com>
2024-09-21 15:57:49 +0600

    add route

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:versatilo_app/config/theme.dart';
+
+import 'config/routes.dart';
+import 'config/theme.dart';
+import 'core/constants/route_constants.dart';
+import 'core/constants/string_constants.dart';
 
 class App extends StatelessWidget {
-  const App({super.key});
+  const App({super.key, required this.router});
+
+  final AppRouter router;
 
   @override
   Widget build(BuildContext context) {
     MaterialTheme theme = MaterialTheme();
     return MaterialApp(
-      title: 'Versatilo App',
+      debugShowCheckedModeBanner: false,
+      title: StringConstants.appName,
       themeMode: ThemeMode.light,
       theme: theme.light(),
+      initialRoute: RouteConstants.welcomePage,
+      onGenerateRoute: router.onGenerateRoute,
     );
   }
 }
