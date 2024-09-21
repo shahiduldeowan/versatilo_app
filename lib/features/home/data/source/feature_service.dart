459ea3a78a480b48613eff08c04a13088649361a
Shahidul Deowan <deowanshahidul@gmail.com>
2024-09-21 16:06:46 +0600

    add features

---
+++ lib/features/home/data/source/feature_service.dart
@@ -0,0 +1,35 @@
+import '../../../../core/constants/assets_constants.dart';
+import '../model/feature_model.dart';
+
+List<FeatureModel> features = [
+  const FeatureModel(
+    title: 'Easy to Use',
+    description:
+        'Our app is designed to be user-friendly and easy to navigate, making it simple for you to find what you need.',
+    image: AssetsConstants.easyToUse,
+  ),
+  const FeatureModel(
+    title: 'Fast and Reliable',
+    description:
+        'Our app is built with speed and reliability in mind, ensuring that you can access the information you need quickly and efficiently.',
+    image: AssetsConstants.fastAndReliable,
+  ),
+  const FeatureModel(
+    title: 'Secure and Private',
+    description:
+        'We take the security and privacy of our users seriously, using industry-standard encryption and secure servers to protect your data.',
+    image: AssetsConstants.secureAndPrivate,
+  ),
+  const FeatureModel(
+    title: 'Personalized Experience',
+    description:
+        'Our app is designed to provide a personalized experience, tailoring the content and features to your individual needs and preferences.',
+    image: AssetsConstants.personalizedExperience,
+  ),
+  const FeatureModel(
+    title: 'Real-time Updates',
+    description:
+        'Our app provides real-time updates and notifications, keeping you informed and up-to-date on the latest news and developments.',
+    image: AssetsConstants.realTimeUpdates,
+  ),
+];
